81dbe3b025ac6f7e83573b381843725d10bcdf2c
github username <github email address>
2020-02-21 15:50:42 -0500

    add show attribute to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,18 +3,18 @@ a = Creator.new(username: "Spikehead", password: "password")
 b = Creator.new(username: "Baldy", password: "password")
 
 
-c = Virtpal.new(name: "Kinchlow", color: 'green', height: 'tall', build: 'skinny', mood: 'happy')
+c = Virtpal.new(name: "Kinchlow", color: 'green', height: 'tall', build: 'skinny', mood: 'happy', show: true)
 c.creator = a
 c.save
 
-d = Virtpal.new(name: "Babby", color: 'red', height: 'short', build: 'medium', mood: 'silly')
+d = Virtpal.new(name: "Babby", color: 'red', height: 'short', build: 'medium', mood: 'silly', show: false)
 d.creator = a
 d.save
 
-e = Virtpal.new(name: "Foofoo", color: 'blue', height: 'medium', build: 'fat', mood: 'stoked')
+e = Virtpal.new(name: "Foofoo", color: 'blue', height: 'medium', build: 'fat', mood: 'stoked', show: true)
 e.creator = b
 e.save
 
-f = Virtpal.new(name: "George", color: 'blue', height: 'short', build: 'fat', mood: 'silly')
+f = Virtpal.new(name: "George", color: 'blue', height: 'short', build: 'fat', mood: 'silly', show: false)
 f.creator = b
 f.save
