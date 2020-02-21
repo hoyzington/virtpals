e4200382c4ca65c3be5dd7d78a39de0a17001f7a
github username <github email address>
2020-02-21 01:51:18 -0500

    work on vp rendering

--- app/models/virtpal.rb
+++ app/models/virtpal.rb
@@ -2,36 +2,49 @@ class Virtpal < ActiveRecord::Base
 
   belongs_to :creator
 
-  @@options = {“color” => {“blue” => “.colorize(:light_blue)”,
-                            “red” => “.colorize(:red)”,
-                            “green” => “.colorize(:light_green)”},
-                
-                “height” => {“short” => 0,
-                             “medium” => 1,
-                             “tall” => 2},
-                             
-                “build” => [{:head => {“skinny” => [“ “, [1, 0, 0, 1]],
-                                       “medium” => [“ “, [1, 0, 0, 1]],
-                                       “fat” => [“_”, [0, 1, 1, 0]]}},
-                            {:body => {“skinny” => [2, 0, 0, 2],
-                                       “medium” => [1, 1, 1, 1],
-                                       “fat” => [0, 2, 2, 0]}}],
+  @@options = {"color" => {"blue" => "00F",
+                           "red" => "F00",
+                           "green" => "090"},
+                          
+               "height" => {"short" => 0,
+                            "medium" => 1,
+                            "tall" => 2},
+                          
+               "build" => {:head => {"skinny" => [" ", [" ", ""]],
+                                     "medium" => [" ", [" ", ""]],
+                                     "fat" => ["_", ["", " "]]},
+                           :body => {"skinny" => ["  ", "", ""],
+                                     "medium" => [" ", " ", "_"],
+                                     "fat" => ["", "  ", "__"]}},
+                                    
+               "mood" => {"happy" => [" ^ ^", "  = ", "X", "X"],
+                          "silly" => ["O  ^", "( ~)", "}", "{"],
+                          "stoked" => ["o`'o", " <> ", "d", "b"]}}
 
-                “mood” => {“happy” => [“ ^ ^”, “X”, “  = ”, “X”]
-                           “silly” => [“O  ^”, “}”, “( ~)”, “{“]
-                           “stoked” => [“ > <”, “d”, “ <> ”, “b”]}
-  
-  def initialize
-
+  def self.options
+    @@options
   end
 
   def settings
-
+    @top = @@options["build"][:head][self.build][0]
+    @hs_a = @@options["build"][:head][self.build][1][0]
+    @hs_b = @@options["build"][:head][self.build][1][1]
+    @eyes = @@options["mood"][self.mood][0]
+    @mouth = @@options["mood"][self.mood][1]
+    @bs_a = @@options["build"][:body][self.build][0]
+    @bs_b = @@options["build"][:body][self.build][1]
+    @hand_a = @@options["mood"][self.mood][2]
+    @hand_b = @@options["mood"][self.mood][3]
+    @btm = @@options["build"][:body][self.build][2]
+    @h = @@options["height"][self.height]
+    @color = @@options["color"][self.color]
   end
   
-  def builder(settings)
-
+  def creation
+    build = []
+    build[0] = "          " + @top + "__" + @top
+    build[1] = "         " + @hs_a + "/" + @hs_b + "  " + @hs_b + "\\"
+    build
   end
-
 
 end
